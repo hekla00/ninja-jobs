8d0278e831aa125fde8321f11f804b9ddef31ab6
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2022-01-02 16:40:09 +0100

    Lesson 54-56: Fetching Data Using JSON Server

--- src/views/Jobs/JobDetails.vue
+++ src/views/Jobs/JobDetails.vue
@@ -1,6 +1,10 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The job id is {{ $route.params.id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>Loading job details...</div>
 </template>
 
 <script>
@@ -12,6 +16,17 @@ export default {
   //       id: this.$route.params.id,
   //     };
   //   },
+  data() {
+    return {
+      job: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
+  },
 };
 </script>
 
