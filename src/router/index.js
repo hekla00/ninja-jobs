75cc047df001138ddee6fd35ce02c9c833f1f13d
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2022-01-02 15:50:03 +0100

    Lesson 49: Route Parameters

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About";
 import Jobs from "../views/Jobs/Jobs.vue";
+import JobDetails from "../views/Jobs/JobDetails.vue";
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: "/Jobs",
     name: "Jobs",
     component: Jobs,
+  },
+  {
+    path: "/jobs/:id",
+    name: "JobDetails",
+    component: JobDetails,
   },
 ];
 
--- src/views/Jobs/JobDetails.vue
+++ src/views/Jobs/JobDetails.vue
@@ -1,7 +1,16 @@
-<template>Job details</template>
+<template>
+  <h1>Job Details Page</h1>
+  <p>The job id is {{ $route.params.id }}</p>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+    };
+  },
+};
 </script>
 
 <style></style>
