b2b270f20ab7567301f52cf0a162892356f9eb5f
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2022-01-02 15:34:36 +0100

    Lesson 47: Router Links

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas odio
+      laborum consequatur, et doloremque officia quis possimus accusantium ipsam
+      quaerat eaque maiores vel velit assumenda fugiat culpa exercitationem,
+      numquam deserunt esse nihil ut sequi? Adipisci facilis porro atque
+      consequuntur veniam dolores odio animi? Consequatur, quis fugit qui
+      laudantium et ipsum reiciendis voluptatibus exercitationem nisi atque
+      perspiciatis repudiandae laborum, quasi obcaecati itaque vero, a ab sit
+      ipsa esse id. Dolorem asperiores in reprehenderit adipisci atque placeat
+      nam harum quis eligendi eveniet! Odio tempore nesciunt dignissimos
+      corporis molestias ut amet iure voluptatibus quibusdam assumenda possimus,
+      incidunt, eligendi sequi optio adipisci, asperiores dolores?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas odio
+      laborum consequatur, et doloremque officia quis possimus accusantium ipsam
+      quaerat eaque maiores vel velit assumenda fugiat culpa exercitationem,
+      numquam deserunt esse nihil ut sequi? Adipisci facilis porro atque
+      consequuntur veniam dolores odio animi? Consequatur, quis fugit qui
+      laudantium et ipsum reiciendis voluptatibus exercitationem nisi atque
+      perspiciatis repudiandae laborum, quasi obcaecati itaque vero, a ab sit
+      ipsa esse id. Dolorem asperiores in reprehenderit adipisci atque placeat
+      nam harum quis eligendi eveniet! Odio tempore nesciunt dignissimos
+      corporis molestias ut amet iure voluptatibus quibusdam assumenda possimus,
+      incidunt, eligendi sequi optio adipisci, asperiores dolores?
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam quae
+      et maxime quaerat. Sed placeat quidem, quam veritatis doloremque deleniti
+      ducimus tenetur aperiam est officia distinctio rerum voluptatem similique
+      consequatur!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, neque minima?
+      Consectetur ipsa magnam maxime quibusdam cum nemo praesentium quidem quae.
+      Ea eaque odit possimus commodi aspernatur quaerat nobis, deserunt placeat
+      maiores ex nisi quam, vero facere quia! Adipisci totam porro reiciendis ut
+      non ea inventore odio ipsum, officiis optio deserunt? Dignissimos, minus
+      aliquid voluptatibus quasi a voluptatum. Expedita quae rem eligendi
+      excepturi necessitatibus, ab minima reiciendis accusamus, soluta neque
+      doloremque alias fugit quo nostrum ipsum non iure. Blanditiis temporibus
+      quasi a voluptatem laborum amet laudantium voluptatum placeat sed iste.
+      Quas dicta atque odit totam explicabo repellat ut corporis ea!
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
