d9dcd99573024a972528c2dea24797f0054ed4cf
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2022-01-02 15:41:37 +0100

    Lesson 48: Folder Structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import About from "../views/About";
+import Jobs from "../views/Jobs/Jobs.vue";
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: "/about",
     name: "About",
     component: About,
+  },
+  {
+    path: "/Jobs",
+    name: "Jobs",
+    component: Jobs,
   },
 ];
 
